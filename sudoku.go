7f141bc72811a452d7ddfc3204f875bc08ea5fa4
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-07 11:21:13 +0530

    Add type SudokuGrid

--- sudoku.go
+++ sudoku.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/rivo/tview"
+)
+
 type SudokuCell struct {
 	value    byte
 	readonly bool
@@ -40,4 +44,35 @@ func (c *SudokuCell) SetReadonly(v bool) *SudokuCell {
 
 func (c SudokuCell) Rune() rune {
 	return rune(c.value)
+}
+
+type SudokuGrid struct {
+	*tview.Box
+	selectedRow, selectedColumn int
+	contents                    [81]*SudokuCell
+}
+
+// NewSudokuGrid returns a new SudokuGrid.
+func NewSudokuGrid() *SudokuGrid {
+	var contents [81]*SudokuCell
+	for r := 0; r < 9; r++ {
+		for c := 0; c < 9; c++ {
+			contents[9*r+c] = NewSudokuCell('0')
+		}
+	}
+	return &SudokuGrid{
+		Box:      tview.NewBox(),
+		contents: contents,
+	}
+}
+
+// SelectCell focuses the cell at row r and column c.
+func (g *SudokuGrid) SelectCell(r, c int) *SudokuGrid {
+	g.selectedRow, g.selectedColumn = r, c
+	return g
+}
+
+// GetCell returns the cell at row r and column c.
+func (g *SudokuGrid) GetCell(r, c int) *SudokuCell {
+	return g.contents[9*r+c]
 }
