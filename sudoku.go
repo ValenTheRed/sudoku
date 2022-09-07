83276e69f724013a333c8ffc006d0ff6c31a821c
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-07 11:24:03 +0530

    Implement InputHandler for SudokuGrid

--- sudoku.go
+++ sudoku.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
@@ -75,4 +76,53 @@ func (g *SudokuGrid) SelectCell(r, c int) *SudokuGrid {
 // GetCell returns the cell at row r and column c.
 func (g *SudokuGrid) GetCell(r, c int) *SudokuCell {
 	return g.contents[9*r+c]
+}
+
+// InputHandler returns the handler for this primitive.
+func (g *SudokuGrid) InputHandler() func(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
+	forward := func(pos *int) func() {
+		return func() {
+			*pos = (*pos + 1) % 9
+		}
+	}
+	backward := func(pos *int) func() {
+		return func() {
+			*pos--
+			if *pos < 0 {
+				*pos += 9
+			}
+		}
+	}
+	down := forward(&g.selectedRow)
+	up := backward(&g.selectedRow)
+	left := backward(&g.selectedColumn)
+	right := forward(&g.selectedColumn)
+	return g.WrapInputHandler(func(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
+		switch key := event.Key(); key {
+		case tcell.KeyRune:
+			switch r := event.Rune(); r {
+			case 'j':
+				down()
+			case 'k':
+				up()
+			case 'h':
+				left()
+			case 'l':
+				right()
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				cell := g.GetCell(g.selectedRow, g.selectedColumn)
+				if !cell.Readonly() {
+					cell.SetValue(byte(r))
+				}
+			}
+		case tcell.KeyDown:
+			down()
+		case tcell.KeyUp:
+			up()
+		case tcell.KeyLeft:
+			left()
+		case tcell.KeyRight:
+			right()
+		}
+	})
 }
