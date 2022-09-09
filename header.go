26926b0d722fa319ef4d0269c8cc04148dac40e2
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-09 13:12:47 +0530

    Implement type Timer

--- header.go
+++ header.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
@@ -35,6 +39,47 @@ func (h *SudokuHeader) Draw(screen tcell.Screen) {
 	}
 }
 
+// Timer counts the number of seconds it took to complete the puzzle.
+//
+// I've not included any way to check whether the Timer is running or
+// not since, Timer will start at the beginning of the application and
+// stop only when the puzzle is correctly completed or the application
+// exits.
+type Timer struct {
+	*SudokuHeader
+	elapsed second
+	stopCh  chan struct{}
+}
+
+// NewTimer returns a new initialised Timer.
+//
+// NOTE: NewTimer doesn't set handler for queueing redraws via
+// SetChangedFunc(), i.e.
+//	Timer.SetChangedFunc(func() {
+//		app.Draw()
+//	})
+// users of NewTimer have to do that themselves. Without installing the
+// handler, Timer text would not be updated.
+func NewTimer() *Timer {
+	t := &Timer{
+		SudokuHeader: NewSudokuHeader(),
+		stopCh:       make(chan struct{}),
+	}
+	t.SetText(t.elapsed.String())
+	return t
+}
+
+func (t *Timer) Start() {
+	go worker(func() {
+		t.elapsed++
+		t.SetText(t.elapsed.String())
+	}, t.stopCh)
+}
+
+func (t *Timer) Stop() {
+	t.stopCh <- struct{}{}
+}
+
 type second int
 
 func (s second) String() string {
@@ -51,4 +96,20 @@ func (s second) String() string {
 	}
 	ret.WriteString(fmt.Sprintf("%ds", sec))
 	return ret.String()
+}
+
+// Worker executes work after every second. If a message is sent to
+// quit, Worker returns.
+func worker(work func(), quit <-chan struct{}) {
+	t := time.NewTicker(1 * time.Second)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-t.C:
+			work()
+		case <-quit:
+			return
+		}
+	}
 }
