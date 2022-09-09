400c76c46656f01960ffa34f73f7010b8ac11d15
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-09 18:37:19 +0530

    Implement NewSudokuFrame()

--- frame.go
+++ frame.go
@@ -10,3 +10,19 @@ type SudokuFrame struct {
 	timer      *Timer
 	grid       *SudokuGrid
 }
+
+func NewSudokuFrame() *SudokuFrame {
+	f := &SudokuFrame{
+		Grid: tview.NewGrid(),
+		grid: GeneratePuzzle(),
+	}
+	f.difficulty = NewSudokuHeader(f)
+	f.timer = NewTimer(f)
+	f.difficulty.SetText("Difficulty")
+	f.
+		SetRows(2, 0).SetColumns(0, 0).
+		AddItem(f.timer, 0, 1, 1, 1, 0, 0, false).
+		AddItem(f.difficulty, 0, 0, 1, 1, 0, 0, false).
+		AddItem(f.grid, 1, 0, 1, 2, 0, 0, true)
+	return f
+}
