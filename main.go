eceadc959a5c9e8b4eb47dff5a33a7e421e59fa1
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-13 01:20:15 +0530

    Solve button triggers a confirmation popup window

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ func main() {
 	sidepane := NewSidepane()
 
 	// Restart this game
+	solveModal := NewModal()
+	InitModalStyle(solveModal)
+	solveModal.SetText("Do you want to solve this game?")
+	solveModal.AddButtons([]string{"Cancel", "Yes"})
+
+	// Restart this game
 	restartModal := NewModal()
 	InitModalStyle(restartModal)
 	restartModal.SetText("Do you want to restart this game?")
@@ -37,6 +43,7 @@ func main() {
 				t = LightColorScheme
 			}
 			SetTheme(t)
+			InitModalStyle(solveModal)
 			InitModalStyle(restartModal)
 			InitModalStyle(validateModal)
 			app.Draw()
@@ -51,6 +58,7 @@ func main() {
 	pages := tview.NewPages()
 	pages.AddPage("grid", grid, true, true)
 	pages.AddPage("restart", restartModal, true, false)
+	pages.AddPage("solve", solveModal, true, false)
 	pages.AddPage("validate", validateModal, true, false)
 	sidepane.Buttons[1].SetSelectedFunc(func() {
 		pages.ShowPage("validate")
@@ -58,6 +66,15 @@ func main() {
 	validateModal.SetDoneFunc(func(buttonIndex int, buttonLabel string) {
 		if buttonLabel == "Yes" {
 			// TODO: validating logic
+		}
+		pages.SwitchToPage("grid")
+	})
+	sidepane.Buttons[2].SetSelectedFunc(func() {
+		pages.ShowPage("solve")
+	})
+	solveModal.SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+		if buttonLabel == "Yes" {
+			// TODO: auto-solve logic
 		}
 		pages.SwitchToPage("grid")
 	})
