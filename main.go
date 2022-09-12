658262c1530711d9c44409986512f17d9d23e9de
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-12 22:11:44 +0530

    Restart button triggers a confirmation popup window

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 func main() {
 	SetTheme(DarkColorScheme)
 
-	app := tview.NewApplication()
-	app.EnableMouse(true)
+	app := tview.NewApplication().EnableMouse(true)
 
 	frame := NewSudokuFrame()
+	frame.timer.SetChangedFunc(func() {
+		app.Draw()
+	})
+
 	sidepane := NewSidepane()
+	// Restart this game
+	restartModal := tview.NewModal()
+	restartModal.SetText("Do you want to restart this game?").
+		AddButtons([]string{"Cancel", "Yes"})
 	// Theme changer
 	sidepane.Buttons[4].SetSelectedFunc(func() {
 		go func() {
@@ -24,17 +31,26 @@ func main() {
 		}()
 	})
 
-	root := tview.NewGrid().
-		SetRows(0).SetColumns(-1, -3).
+	grid := tview.NewGrid()
+	grid.SetRows(0).SetColumns(-1, -3).
 		AddItem(sidepane, 0, 0, 1, 1, 0, 0, false).
 		AddItem(frame, 0, 1, 1, 1, 0, 0, true)
 
-	frame.timer.SetChangedFunc(func() {
-		app.Draw()
+	pages := tview.NewPages()
+	pages.AddPage("grid", grid, true, true)
+	pages.AddPage("restart", restartModal, true, false)
+	sidepane.Buttons[3].SetSelectedFunc(func() {
+		pages.ShowPage("restart")
+	})
+	restartModal.SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+		if buttonLabel == "Yes" {
+			// TODO: restarting logic
+		}
+		pages.SwitchToPage("grid")
 	})
 
 	frame.timer.Start()
-	if err := app.SetRoot(root, true).SetFocus(root).Run(); err != nil {
+	if err := app.SetRoot(pages, true).SetFocus(pages).Run(); err != nil {
 		panic(err)
 	}
 }
